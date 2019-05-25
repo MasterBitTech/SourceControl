f76872558c6840ceea4c6edeb3994af54069ed0f
MasterBitTech <cauchy.benoit@icloud.com>
2019-05-24 21:39:23 -0400

    Premier commit

---
+++ SourceControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SourceControl
+//
+//  Created by Benoit Cauchy on 2019-05-24.
+//  Copyright © 2019 Benoit Cauchy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
